82e11a322925a3fab07a725f3201fbfde98fceb8
void059 <132476474+void059@users.noreply.github.com>
2023-09-03 13:28:34 +0100

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
 // Your First Program
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
     }
-}
+};
