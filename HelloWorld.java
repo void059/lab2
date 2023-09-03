c9d0467c00e80d75103bd727e399aa92420d3e2b
void059 <132476474+void059@users.noreply.github.com>
2023-09-03 13:43:13 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ class HelloWorld {
 	// Prints "Hello Edureka" to the terminal windows
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to Edureka poll Online Training!!");
+		System.out.println("Welcome to Edureka poll test Online Training!!");
 	}
 }
