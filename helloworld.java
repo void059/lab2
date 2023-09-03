6812bf95cbd97979ee4507977dd23c321b2d7d10
void059 <132476474+void059@users.noreply.github.com>
2023-09-03 13:34:23 +0100

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,11 @@
+//This is a simple java program
+//FileName: "HelloWorld.java"
+
 class HelloWorld {
-    // Your program begins with a call to main().
-    // Prints "Hello, World" to the terminal window.
-    public static void main(String args[])
-    {
-        System.out.println("Hello, World");
-    }
+	// Your program begins with a call to main().
+	// Prints "Hello Edureka" to the terminal windows
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Edureka Jenkins's Online Training!!");
+	}
 }
